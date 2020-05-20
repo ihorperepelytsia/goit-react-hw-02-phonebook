1b0d326bd262aa2e000a4993a294351fd38b2334
Ihor Perepelytsia <igorekperepelitsa@gmail.com>
2020-05-20 09:47:59 +0300

    done phonebook

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Form from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import ContactFilter from './ContactFilter/ContactFilter';
+import filterContact from '../utils/filterContact';
+import { v4 } from 'uuid';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  addContact = array => {
+    const arrayToAdd = {
+      id: v4(),
+      ...array,
+      // name: array.name,
+      // number: array.number.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1-'),
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, arrayToAdd],
+    }));
+  };
+
+  deleteContact = ({ target: { name } }) => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts.filter(item => item.id !== name)],
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = filterContact(contacts, filter);
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form onAddContact={this.addContact} contacts={contacts} />
+
+        <h2>Contacts</h2>
+        {contacts.length >= 2 && (
+          <ContactFilter value={filter} onChangeFilter={this.changeFilter} />
+        )}
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
